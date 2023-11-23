8577aae58091555912b60178a703c49d03ac91ed
Bkrsingh <148036497+Bkrsingh@users.noreply.github.com>
2023-11-23 11:38:41 +0530

    add  new function is added

--- src/main/java/demo/sl/MyClass.java
+++ src/main/java/demo/sl/MyClass.java
@@ -17,11 +17,15 @@ public class MyClass {
 	{
 		return a/b;
 	}
+	public int add(int a, int b)
+	{
+		return a+b;
+	}
 	public static void main(String[] args) {		
 		MyClass ob = new MyClass();
 		System.out.println("Sum is : "+ob.sum(20, 10));
 		System.out.println("Difference is : "+ob.sub(20, 10));
 		System.out.println("Multiply is : "+ob.mul(20, 10));
 		System.out.println("Division is : "+ob.div(20, 10));
-	}
+	}       System.out.println("Sum is : "+ob.sum(30, 10));
 }
