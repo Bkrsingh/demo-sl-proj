a5eb867fd62d63a374098cdc600e7ea761b6089b
Bkrsingh <148036497+Bkrsingh@users.noreply.github.com>
2023-11-23 11:46:50 +0530

    mul is add new function

--- src/main/java/demo/sl/MyClass.java
+++ src/main/java/demo/sl/MyClass.java
@@ -17,9 +17,9 @@ public class MyClass {
 	{
 		return a/b;
 	}
-	public int add(int a, int b)
+	public int mul(int a, int b)
 	{
-		return a+b;
+		return a*b;
 	}
 	public static void main(String[] args) {		
 		MyClass ob = new MyClass();
@@ -27,5 +27,6 @@ public class MyClass {
 		System.out.println("Difference is : "+ob.sub(20, 10));
 		System.out.println("Multiply is : "+ob.mul(20, 10));
 		System.out.println("Division is : "+ob.div(20, 10));
-	}       System.out.println("Sum is : "+ob.sum(30, 10));
+		System.out.println("Multiply is : "+ob.mul(20, 10));
+	}
 }
